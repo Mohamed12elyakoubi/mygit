a9a65503d96babeb266d38e398238fa9de245340
mohamed23-2000 <elyakoubi412@gmail.com>
2023-05-08 10:24:27 +0200

    commit 1

---
+++ week1.php
@@ -0,0 +1,25 @@
+<?php
+// onderdeel1
+// $variabele1 = 12;
+// $variabele2 = 10;
+// $uitkomst = $variabele1 + $variabele2;
+// echo $uitkomst;
+
+//onderdeel2--------------------------------------------------------
+// $variabele1 = 12;
+// $variabele2 = 10;
+// $uitkomst = $variabele1 * $variabele2;
+// echo $uitkomst;
+
+//onderdeel3--------------------------------------------------------
+
+// $naam = "Mohamed";
+// echo "Welkom " . $naam;
+
+//onderdeel4--------------------------------------------------------
+
+// $mijnNaam = "Carl";
+// $jouwNaam = "Camel";
+// echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
